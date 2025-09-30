b74e265c486cceaf721e0672b89d9f7dca3dfa1c
KiwwYs <nattakronpoom48@gmail.com>
2025-09-30 23:17:04 +0700

    finish

--- Scripts/GameManager.gd
+++ Scripts/GameManager.gd
@@ -5,6 +5,8 @@ extends Node3D
 var score = 0
 
 # ---------- FUNCTIONS ---------- #
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 
 func _process(_delta):
 	show_mouse_cursor()
@@ -21,3 +23,8 @@ func add_score():
 
 func show_win():
 	get_node("/root/DemoScene/UserInterface/Win").visible = true
+	
+	
+func _on_button_pressed():
+	get_node("/root/DemoScene/UserInterface/Start").visible = false
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
